8703425094f25eb18f5a8b14cb1d2b71fa70886c
Julian Jara-Ettinger <julianjara-ettinger@Eco.local>
2015-02-26 22:49:07 -0500

    V0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
 		return f.read()
 
 setup(name='bishop',
-	version='0.01',
+	version='0.1',
 	description='Bayesian Theory of Mind library',
 	url='http://gibthub.com/julianje/bishop',
 	author='Julian Jara-Ettinger',
