eae746328c06cd2dc5d9f7ea92314cfd5542e980
julianje <jjara@mit.edu>
2015-03-06 21:08:01 -0500

    Updating examples because variable name changed

--- Bishop/Examples/SimpleInference.py
+++ Bishop/Examples/SimpleInference.py
@@ -2,5 +2,5 @@
 import Bishop
 Observer = Bishop.LoadEnvironment("Tatik_T1")
 ObservedPath = Observer.M.GetActionList(['R'])
-Res = Observer.InferAgent(StartingPoint=[6,6], ActionSequence=ObservedPath, Samples=50)
+Res = Observer.InferAgent(StartingCoordinates=[6,6], ActionSequence=ObservedPath, Samples=50)
 Res.Summary()
