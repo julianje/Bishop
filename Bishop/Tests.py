baa2006b0df85016d798fc8f520a5044cd9218b3
julianje <jjara@mit.edu>
2015-05-19 11:40:09 -0400

    Common code that I use

--- Bishop/Tests.py
+++ Bishop/Tests.py
@@ -1,6 +1,10 @@
 from Bishop import *
 import numpy as np
-# Test MDP.
+
+############
+# Test MDP #
+############
+
 # Create a one-dimension MDP manually
 S = range(5)
 A = range(2)  # left or right
@@ -20,3 +24,40 @@ T[3][1][4] = 1
 T[4][1][4] = 1
 Test = MDP.MDP(S, A, T, R)
 Test.validate()
+
+############
+# Test Map #
+############
+
+# Create a simple grid world
+Test = Map()
+Test.BuildGridWorld(3, 4, diagonal=True)
+Test.PrintMap()
+Test.Validate()  # Should fail.
+Test.InsertObjects([1, 2, 4], [0, 0, 1], ["A", "B"])
+Test.AddStartingPoint(10)
+Test.AddExitState(0)
+Test.Validate()  # Success!
+
+# Repeat above, but do not number object types correctly.
+Test = Map()
+Test.BuildGridWorld(3, 4, diagonal=False)
+Test.Validate()  # Should fail.
+Test.InsertObjects([1, 2, 4], [1, 1, 2], ["A"])  # Fail
+Test.InsertObjects([1, 2, 4], [1, 1, 2], ["A", "B"])  # Ok
+Test.AddStartingPoint(10)
+Test.AddExitState(0)
+Test.Validate()  # Should fail
+
+# Repeat above, but using coordinate system
+Test = Map()
+Test.BuildGridWorld(3, 4, diagonal=False)
+Test.PrintMap()
+Test.Validate()  # Should fail.
+S1 = Test.GetRawStateNumber([2, 1])
+S2 = Test.GetRawStateNumber([3, 1])
+S3 = Test.GetRawStateNumber([2, 2])
+Test.InsertObjects([S1, S2, S3], [0, 0, 1], ["A", "B"])
+Test.AddStartingPoint(10)
+Test.AddExitState(0)
+Test.Validate()  # Success!
