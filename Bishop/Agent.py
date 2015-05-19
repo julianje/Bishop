e3249a792b837fe29408110e27905f7ec78a9936
julianje <jjara@mit.edu>
2015-05-19 11:51:58 -0400

    2.0 Compatibility
    * Sampling parameters are now lists rather than integers
    * Added scaled uniform and gaussian priors
    * Minor documentation updates

--- Bishop/Agent.py
+++ Bishop/Agent.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """
-Markov Decision Process solver.
+Stores information about agent and comes with supporting methods to sample random agents.
 """
 
+__author__ = "Julian Jara-Ettinger"
 __license__ = "MIT"
 
 import random
@@ -12,31 +13,38 @@ import numpy as np
 
 class Agent(object):
 
-    def __init__(self, Map, Prior="Exponential", CostParam=0.1, RewardParam=10):
+    def __init__(self, Map, Prior, CostParams, RewardParams):
         """
         Agent class.
 
-        Create an agent who has costs and rewards.
+        Create an agent with a set of costs and rewards.
+        If sampling parameters are numbers rather than lists the constructor fixes this automatically.
 
         Args:
             Map (Map): A map object.
-            Prior (str): String indicating prior's name (Only Simplex and Exponential supported)
-            CostParam (float): Parameter for sampling costs
-            RewardParam (float): Parameter for sampling rewards
+            Prior (str): String indicating prior's name. Run Agent.Priors() to see list
+            CostParams (list): List of parameters for sampling costs
+            RewardParams (list): List of parameters for sampling rewards
         """
         self.CostDimensions = len(np.unique(Map.StateTypes))
         # Get dimensions over which you'll build your simplex
         self.RewardDimensions = sum([i > 0 for i in map(len, Map.Locations)])
         if self.RewardDimensions == 0:
-            print "WARNING: No rewards on map."
-        self.CostParam = CostParam
-        self.RewardParam = RewardParam
+            print "WARNING: No rewards on map. AGENT-001"
+        if isinstance(CostParams, list):
+            self.CostParams = CostParams
+        else:
+            self.CostParams = [CostParams]
+        if isinstance(RewardParams, list):
+            self.RewardParams = RewardParams
+        else:
+            self.RewardParams = [RewardParams]
         self.ResampleCosts()  # Generate random cost of map
         self.ResampleRewards()  # Generate random rewards for objects
 
     def ResampleAgent(self, Apathy=0, Restrict=False):
         """
-        Reset the agent with random parameters.
+        Reset agent with random costs and rewards.
 
         Args:
             Apathy (float): Probability that agent doesn't like objects.
@@ -66,7 +74,7 @@ class Agent(object):
         """
         # Resample the agent's competence
         self.costs = self.Sample(
-            self.CostDimensions, self.CostParam, Kind=self.Prior)
+            self.CostDimensions, self.CostParams, Kind=self.Prior)
         self.costs = [0 if random.random() < Apathy else i for i in self.costs]
 
     def ResampleRewards(self, Apathy=0):
@@ -81,7 +89,7 @@ class Agent(object):
         """
         # Resample the agent's preferences
         self.rewards = self.Sample(
-            self.RewardDimensions, self.RewardParam, Kind=self.Prior)
+            self.RewardDimensions, self.RewardParams, Kind=self.Prior)
         self.rewards = [
             0 if random.random() < Apathy else i for i in self.rewards]
 
@@ -97,14 +105,24 @@ class Agent(object):
         Returns:
             None
         """
-        if(Kind == "Simplex"):
+        if (Kind == "Simplex"):
             # Output: Simplex sample of length 'dimensions' (Adds to 1)
             sample = -np.log(np.random.rand(dimensions))
             return sample / sum(sample)
-        if(Kind == "Exponential"):
+        if (Kind == "ScaledUniform"):
+            return np.round(np.random.rand(dimensions)) * SamplingParam[0]
+        if (Kind == "Gaussian"):
+            return np.random.normal(SamplingParam[0], SamplingParam[1], dimensions)
+        if (Kind == "Exponential"):
             sample = [
-                np.random.exponential(SamplingParam) for j in range(dimensions)]
+                np.random.exponential(SamplingParam[0]) for j in range(dimensions)]
             return sample
+
+    def Priors(self):
+        """
+        Print list of supported priors. This is hardcoded for now.
+        """
+        print ("Simplex, ScaledUniform, Gaussian, Exponential")
 
     def Display(self, Full=True):
         """
