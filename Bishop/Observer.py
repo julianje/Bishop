22df78848789581bb4f28a3e22fd3e7f37272c48
julianje <jjara@mit.edu>
2015-05-21 10:59:31 -0400

    Observer 2.0
    * Confirmed Likelihood works
    * Better SimulateAgents function
    * InferAgent() done

--- Bishop/Observer.py
+++ Bishop/Observer.py
@@ -18,18 +18,47 @@ import time
 class Observer(object):
 
     def __init__(self, A, M):
+        """
+        Build an observed object
+
+        Args:
+            A (Agent): Agent object
+            M (Map): Map objects
+        """
         self.Plr = Planner.Planner(A, M)
 
     def InferAgent(self, ActionSequence, Samples):
-        return None
+        """
+        Compute a series of samples with their likelihoods.
 
-    def SimulateAgents(self, Samples, HumanReadable=False, Simple=True):
+        Args:
+            ActionSequence (list): Sequence of actions
+            Samples (int): Number of samples to use
+        """
+        Costs = [0] * Samples
+        Rewards = [0] * Samples
+        LogLikelihoods = [0] * Samples
+        for i in range(Samples):
+            # Propose a new sample
+            self.Plr.Agent.ResampleAgent()
+            Costs[i] = self.Plr.Agent.costs
+            Rewards[i] = self.Plr.Agent.rewards
+            # Replan
+            self.Plr.BuildPlanner(False)  # use True to run validation on each object
+            self.Plr.ComputeUtilities()
+            # Get log-likelihood
+            LogLikelihoods[i] = self.Plr.Likelihood(ActionSequence)
+        return [Costs, Rewards, LogLikelihoods]
+
+    def SimulateAgents(self, Samples, HumanReadable=False, ResampleAgent=True, Simple=True):
         """
         Simulate agents navigating through the map.
 
         Args:
             Samples (int): Number of agents to Simulate
             HumanReadable (bool): When true, function prints action names rather than action ids.
+            ResampleAgent (bool): When true simulation uses the same agent.
+                                  If agent is not softmaxing utilities or actions then all action sequences will be identical.
             Simple (bool): When the agent finds more than one equally-valuable action is takes one at random.
                             If simple is set to true it instead chooses the first action in the set.
                             This avoid generating a lot of equivalent paths that look superficially different.
@@ -37,9 +66,10 @@ class Observer(object):
         sys.stdout.write("Costs,Rewards,Actions,States\n")
         for i in range(Samples):
             # Reset agent
-            self.Plr.Agent.ResampleAgent()
+            if ResampleAgent:
+                self.Plr.Agent.ResampleAgent()
             # Replan
-            self.Plr.BuildPlanner(False)  # Switch to true to force validation on each object
+            self.Plr.BuildPlanner(False)  # use True to run validation on each object
             self.Plr.ComputeUtilities()
             # Simulate
             [A, S] = self.Plr.Simulate(Simple)
