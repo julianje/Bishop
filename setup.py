cc72985f1617cf7157404fdbb62211eafe2fb774
julianje <jjara@mit.edu>
2016-10-10 17:52:46 -0400

    Removing code we don't need any longer

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='Bishop',
-      version='2.6.1',
+      version='2.6.2',
       description='Cognitive model of Theory of Mind',
       long_description=readme(),
       url='http://gibthub.com/julianje/bishop',
